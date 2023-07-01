4d9813992e0b928ad54ddf21bd58c52d23b2bc6b
serzh <gerasim4ukserzh@googlemail.com>
2023-07-01 22:55:40 +0300

    fix test settings

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :pxblog, Pxblog.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "user",
+  password: "password",
   hostname: "localhost",
   database: "pxblog_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
